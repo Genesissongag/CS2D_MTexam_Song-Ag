c03f9fbc14ff8f41ded4623e9f30346c90035866
Genesissongag <genesissongag@gmail.com>
2024-10-31 17:30:11 +0800

    Create Userprofile.js

---
+++ Userprofile.js
@@ -0,0 +1,12 @@
+// Collect user information
+let fullName = prompt("Please enter your full name (first and last name):");
+let age = prompt("Please enter your age:");
+let favoriteNumber = prompt("Please enter your favorite number:");
+let favoriteColor = prompt("Please enter your favorite color:");
+
+// Log all collected information to the console
+console.log("User  Information:");
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
